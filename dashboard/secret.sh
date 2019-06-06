42a734db19dc89e223b545169510420faf6c9844
Yunchao Wang <wangyunchao@sjtu.edu.cn>
2019-06-06 12:08:15 +0800

    test

--- dashboard/secret.sh
+++ dashboard/secret.sh
@@ -1,2 +1,4 @@
 kubectl delete secret kubernetes-dashboard-certs -n kube-system
 kubectl create secret generic kubernetes-dashboard-certs --from-file=~/ca -n kube-system
+kubectl delete secret dashboard-tls -n kube-system
+kubectl create secret tls dashboard-tls --key ~/ca/tls.key --cert ~/ca/tls.crt -n kube-system
